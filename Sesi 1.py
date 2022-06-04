77f55ed1dfe1871663b32fc9d5a8c3582c9ba0fa
Arfiah <fiagreen3@gmail.com>
2022-06-04 13:13:40 +0700

    initial commit add sesi 1

---
+++ Sesi 1.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print(123456+1)
+print(1432483489673583295273+1)
+print(10)
+print(type(10))
+print(type(4.))
+
+
+print(4.2)
+print(type(4.2))
+
+print(4.)
+
+print(.2)
+
+print(.4e7)
+
+print(4.2e-4)
+
+
+print("Hacktiv8")
+print(type("Hacktiv8"))
+print('arfiah')
+n = 1000
+print(n)
+
+
+
+
+a=10
+t=20
+print((a*t)/2)
+
+'lanjut'
+
+var="arfiah"
+print(var)
+var = 23.5
+print(var)
+
+var = "Now I'm a string"
+print(var)
+
+'latihan10'
+
+name = 'Arfiah'
+age = 25
+has_laptop = False
+print(name, age, has_laptop)
+
+
+'latihan 11'
+
+age = 10
+Age =11
+a9e =111
+_age =22
+ag3 =33
+_AGE_=9
+print(age,Age,a9e,_age,ag3,_AGE_)
+
+
+'latihan 12'
+a=10
+b=3
+print(a+b)
+
+'latihan 13'
+
+a=10
+b=20
+print(a+b*5)
+print(a+b)
+
+'latihan 13'
+a = 4
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+
+a = 10
+b = 20
+print(a == b)
+
+print(a != b)
+
+print(a <= b)
+
+print(a >= b)
+
+
+a = 30
+b = 30
+print(a == b)
+
+print(a <= b)
+
+print(a >= b)
+
+'latihan 14'
+# + Operators
+s = 'foo'
+t = 'bar'
+u = 'baz'
+
+print(s + t)
+
+print(s + t + u)
+
+
+print('Hacktiv8 ' + 'PTP')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ latihan 3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 10:18:07 2022
+
+@author: Arfiah
+"""
+
+print("Hacktiv8")
+print(type("Hacktiv8"))
+print('arfiah')
+n = 1000
+print(n)
